949c229537c50e5f397c52b203ccb958eff617cb
viscousmink <mitchell.klingler99@gmail.com>
2020-04-13 23:39:37 -0400

    enabled CORS on all endpoints

--- server.js
+++ server.js
@@ -32,6 +32,11 @@ app.get('/swagger.json', function(req, res) {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/api', require('./API/app.js'));
 
 // Serve any static files
